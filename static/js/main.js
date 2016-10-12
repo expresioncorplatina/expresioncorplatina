9d4342b465d6acc2fd01fad8f1ce0a4f7634ee7f
humaarte <humaarte@gmail.com>
2016-10-12 16:29:08 -0500

    'km'

---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+function youtube()
+    {
+        if(document.getElementById('youtube').className == 'youtube')
+            {
+            document.getElementById('youtube').className = 'youtube-show';
+            } else {
+                document.getElementById('youtube').className = 'youtube';
+                    }
+    } 
